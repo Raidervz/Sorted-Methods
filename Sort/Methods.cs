52ba13a7bc508417fd67bfdc6768bc1a2bef2964
Jelby Ottenwalder <Jelby Ottenwalder>
2017-08-13 19:20:54 -0400

    Add project files.

---
+++ Sort/Methods.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    public class Methods
+    {
+
+        public int[] VisualSort(int[] arreglo)
+        {
+            int[] ArregloActual = arreglo.ToArray();
+            
+            int[] ArregloNuevo = new int[arreglo.Length];
+
+            int ArregloTotal = arreglo.Length;
+
+            int marked = 0;
+
+            while (marked != ArregloTotal)
+            {
+                int maxed = -1;
+                int maxedIndex = -1;
+                for (int i = 0; i < ArregloActual.Length; i++)
+                {
+                    if (ArregloActual[i] > maxed)
+                    {
+                        maxed = ArregloActual[i];
+                        maxedIndex = i;
+                    }
+                }
+
+                ArregloNuevo[ArregloTotal - marked - 1] = maxed;
+                ArregloActual[maxedIndex] = -1;
+
+                marked++;
+            }
+
+            return ArregloNuevo;
+        }
+
+        public int[] SelectionSort(int[] arreglo)
+        {
+            int[] responce = new int[arreglo.Length];
+
+            responce = arreglo.ToArray();
+
+            for (int i = 0; i <= responce.Length - 1; i++)
+            {
+                int MaxValueIndex = i;
+                for (int j = i + 1; j <= responce.Length - 1; j++)
+                {
+                    if (responce[j] < responce[MaxValueIndex])
+                    {
+                        MaxValueIndex = j;
+                    }
+                }
+                //Guardar el Valor Actual 
+                int actualValue = responce[i];
+
+                //Reemplazarlo con el Valor menor
+                responce[i] = responce[MaxValueIndex];
+
+                //Poner el Valor Actual en la posicion del Valor Menor
+                responce[MaxValueIndex] = actualValue;
+
+            }
+
+            return responce;
+        }
+
+        public int[] BubbleSort(int[] arreglo)
+        {
+            int[] responce = new int[arreglo.Length];
+
+            responce = arreglo.ToArray();
+
+            for (int i = 1; i < responce.Length - 1; i++)
+            {
+                int stopper = 0;
+                for (int j = 0; j <= responce.Length - i - 1; j++)
+                {
+                    if (responce[j] > responce[j + 1])
+                    {
+                        int actualValue = responce[j];
+                        int nextValue = responce[j + 1];
+
+                        responce[j] = nextValue;
+                        responce[j + 1] = actualValue;
+
+                        stopper = 1;
+                    }
+                }
+                if (stopper == 0) break;
+            }
+
+            return responce;
+        }
+    }
+}
---
+++ Sort/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            int[] MasterIntegerArray = new int[] { 2, 4, 6, 188, 4, 6, 8, 9, 15, 45,  8};
+
+            int[] result;
+
+            Methods _Methods = new Methods();
+
+            Console.WriteLine();
+            Console.Write("Sorted By Visual:    [ ");
+            stopWatch.Start();
+            result = _Methods.VisualSort(MasterIntegerArray);
+            stopWatch.Stop();
+            foreach (var item in result)
+            {
+                Console.Write("{0}, ", item);
+            }
+            Console.WriteLine("]");
+            Console.WriteLine("Time elapsed: {0}", stopWatch.Elapsed);
+            Console.WriteLine();
+
+
+            stopWatch.Reset();
+            Console.WriteLine();
+            stopWatch.Start();
+            result = _Methods.SelectionSort(MasterIntegerArray);
+            stopWatch.Stop();
+            Console.Write("Sorted By Selection: [ ");
+            foreach (var item in result)
+            {
+                Console.Write("{0}, ", item);
+            }
+            Console.WriteLine("]");
+            Console.WriteLine("Time elapsed: {0}", stopWatch.Elapsed);
+            Console.WriteLine();
+
+
+            stopWatch.Reset();
+            Console.WriteLine();
+            stopWatch.Start();
+            result = _Methods.BubbleSort(MasterIntegerArray);
+            stopWatch.Stop();
+            Console.Write("Sorted By BubbleSort: [ ");
+            foreach (var item in result)
+            {
+                Console.Write("{0}, ", item);
+            }
+            Console.WriteLine("]");
+            Console.WriteLine("Time elapsed: {0}", stopWatch.Elapsed);
+            Console.WriteLine();
+
+
+            Console.Read();
+        }
+
+    }
+}
